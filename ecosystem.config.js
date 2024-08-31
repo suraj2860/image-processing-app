21bc70c40880bbe634f40d594044afe375559e4a
Mona Lisa <surajkmr20167@gmail.com>
2024-08-31 11:59:11 +0530

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+export default {
+    apps: [
+      {
+        name: 'redis',
+        script: 'docker',
+        args: 'run -d --name redis -p 6379:6379 redis',
+        interpreter: 'none',
+        watch: false,
+        autorestart: true,
+      },
+    ],
+  };
+  
