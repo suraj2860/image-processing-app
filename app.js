2a8a7dddb0470980f23e097e662b019b7bb25fd0
Mona Lisa <surajkmr20167@gmail.com>
2024-08-31 09:44:11 +0530

    update

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const app = express();
 app.use('/api', uploadRoutes);
 app.use('/api', statusRoutes);
 
+app.get('/', (req, res) => {
+    res.send('image-processing-app is running...');
+});
+
 // Set up the image processor
 setupImageProcessor();
 
